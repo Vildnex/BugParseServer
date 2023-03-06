e9bb8538f94a7076f21572f7586d5ed494d99270
vlad <vladdum94@yahoo.com>
2023-03-06 21:21:03 +0100

    init conf

---
+++ docker_files/parse-server/cloud/main.js
@@ -0,0 +1,7 @@
+Parse.Cloud.define("hello", () => {
+    return "Hello world!";
+});
+
+// Parse.Cloud.define("something", (req) => {
+//     return Parse.Promise.error({"message": "User not found"});
+// });
